d364e26d3f48dde3b5660bf7c4949bfe781170ab
Sophie Stammers <sophie.stammers@gmail.com>
2020-12-07 12:54:10 +0000

    redux installed player 1 increment action logs to console

--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+
+const initial = {
+  player1: 0,
+  player2: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "PLAYER1_INCREMENT": return { 
+      ...state, 
+      player1: state.player1 + 1 
+    };
+    default: return state;
+  }
+};
+
+const store = createStore(reducer, initial);
+
+// subscribe to changes
+store.subscribe(() => {
+  let state = store.getState();
+  // for now, just log the new count
+  console.log(state.player1, state.player2);
+});
+
+// trigger a player 1 increment action
+store.dispatch({ type: "PLAYER1_INCREMENT" });
 
 ReactDOM.render(
   <React.StrictMode>
