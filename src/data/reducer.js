fb85786f5e383f882e8a94825feede81523a8baf
Sophie Stammers <sophie.stammers@gmail.com>
2020-12-08 12:21:18 +0000

    Adding destructuring and tidying reducer helper functions

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,25 +1,29 @@
 import initial from "./initial";
 
-// reducer helper functions
+// helper functions
 const player1 = state => ({ ...state, player1: state.player1 + 1 });
+
 const player2 = state => ({ ...state, player2: state.player2 + 1 });
 
 const server = state => { 
-  // const {player1, player2, player1Serving} = state;
+  const {player1, player2, player1Serving} = state;
+  // If both players pass 20 points, alternate server every 2 points i
   if ((state.player1 >= 20) && (state.player2 >= 20)) {
     return {
       ...state,
-      player1Serving: (state.player1 + state.player2) % 2 === 0 ? !state.player1Serving : state.player1Serving
+      player1Serving: (player1 + player2) % 2 === 0 ? !player1Serving : player1Serving
     }
   }
+  // Alternate server every 5 points 
   return {
     ...state,
-    player1Serving: (state.player1 + state.player2) % 5 === 0 ? !state.player1Serving : state.player1Serving
+    player1Serving: (player1 + player2) % 5 === 0 ? !player1Serving : player1Serving
   } 
 }
 
 const winner = state => {
   const {player1, player2} = state;
+  // To win, a player must (i) reach 21 points, (ii) be at least 2 points ahead
   if ((player1 >= 21) && (player2 <= (player1 - 2))) {
     return {
       ...state,
